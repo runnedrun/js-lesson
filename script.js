d63b89b5d612f415e23e44e9de58c09a4ca10ea4
David Gaynor <runnedrun@gmail.com>
2016-10-10 09:28:52 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-function whenClicked() {
-  console.log("running function")
-  return "1111"
-}
-
-var a = whenClicked()
-
-console.log("running function")
-return "1111"
-
-
-var b = whenClicked
-
-console.log(a())
-console.log(b())
+$("#button").click(
